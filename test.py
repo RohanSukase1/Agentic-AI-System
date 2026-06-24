9ea1cbf7769d83d65bceb9ca70153027dc96ec2c
Rohan Sukase <rohansukase1@gmail.com>
2026-06-24 23:04:25 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+import os
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key="")
+messages = [
+    {"role": "system", "content": "You are a helpful AI assistant."},
+    {"role": "user", "content": "What is AI?"}
+]
+
+response = client.chat.completions.create(
+    model="gpt-4.1",
+    messages=messages
+)
+
+print(response.choices[0].message.content)
